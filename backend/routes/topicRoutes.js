3568ebd61a1a398fd168d6b674d203f6f65a3573
Maathavan <maathumaathavan@gmail.com>
2022-04-22 11:15:38 +0530

    re

--- backend/routes/topicRoutes.js
+++ backend/routes/topicRoutes.js
@@ -9,8 +9,4 @@ router.route('/my').get(protect,getMyTopics)
 router.route('/').post(protect ,setTopic)
 router.route('/:id').get(getTopic)
 router.route('/:id').put(updateTopic).delete(deleteTopic)
-
-router.route('/:id/comments').get(getComments)
-router.route('/:id').post(protect, addComment)
-router.route('/:id/:cid').delete(protect, deleteComment)
 module.exports = router 
