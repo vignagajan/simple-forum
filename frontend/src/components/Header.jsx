f4683c07fbb195b4050ec719447cceeb1077cd92
vignagajan <my.soclntwrks@gmail.com>
2022-04-25 12:21:59 +0530

    Fix header toggle

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react';
 import {Link} from 'react-router-dom';
 
 function Header() {
+
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
       <div className="container ms-auto mb-2 mb-lg-0">
       <Link className="navbar-brand" to="/">Forum</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
-        </button>  
-        <div className="collapse navbar-collapse" id="navbar">
+        </button>   */}
+      <button className="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbar">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <Link className="nav-link" to="/">Home</Link>
@@ -24,6 +34,7 @@ function Header() {
         </div>
       </div>
     </nav>
+    <br /> <br /> <br />
     </>
   )
 }
